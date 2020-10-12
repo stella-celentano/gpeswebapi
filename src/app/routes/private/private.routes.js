26797ca286648136842ac50dfec6f52dbe2f8908
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-12 00:33:51 -0300

    Importando as rotas de Eventos para o endpoint da API.

--- src/app/routes/private/private.routes.js
+++ src/app/routes/private/private.routes.js
@@ -5,10 +5,12 @@ const UserRoutes = require('./routes/usuario.private')
 const ValidatorRoutes = require('./routes/validators.private')
 const AnalyticsRoutes = require('./routes/analytics.private')
 const ContatoRoutes = require('./routes/contato.private')
+const EventoRoutes = require('./routes/eventos.private')
 
 app.use('/usuario', UserRoutes)
 app.use('/validators', ValidatorRoutes)
 app.use('/analytics', AnalyticsRoutes)
 app.use('/contato', ContatoRoutes)
+app.use('/eventos', EventoRoutes)
 
 module.exports = app
