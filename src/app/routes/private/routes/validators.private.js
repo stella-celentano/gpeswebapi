e832578248490c0382319399e99f6834d2fce997
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-13 17:17:30 -0300

    Implementando as rotas dos validadores de categories e publicações

--- src/app/routes/private/routes/validators.private.js
+++ src/app/routes/private/routes/validators.private.js
@@ -5,5 +5,7 @@ const UniqueValidators = require('../../../validators/unique')
 route.get('/unique/usuario/nome', UniqueValidators.uniqueUsuarioNome)
 route.get('/unique/usuario/usuario', UniqueValidators.UniqueUsuarioUsuario)
 route.get('/unique/usuario/email', UniqueValidators.UniqueUsuarioEmail)
+route.get('/unique/categoria/nome', UniqueValidators.uniqueCategoriaNome)
+route.get('/unique/publicacoes/titulo', UniqueValidators.uniquePublicacaoTitulo)
 
 module.exports = route
