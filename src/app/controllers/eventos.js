3135881d4a68907cfd2049c4d5e7802dcef38d52
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-13 11:52:24 -0300

    Implementando o getWithParams e criando o validador unico de titulo.

--- src/app/controllers/eventos.js
+++ src/app/controllers/eventos.js
@@ -2,16 +2,46 @@ const EventoSchema = require('./../models/eventos')
 
 class Evento {
 
-  create(req, res){
+  create(req, res) {
     const body = req.body
 
     EventoSchema.create(body, (err, data) => {
       if (err) {
-        res.status(500).send({ message: 'Houve um erro ao processar sua requisição', error: err })
+        res.status(500).send({
+          message: 'Houve um erro ao processar sua requisição',
+          error: err
+        })
       } else {
-        res.status(201).send({ message: "Evento criado com sucesso no banco de dados", evento: data })
+        res.status(201).send({
+          message: "Evento criado com sucesso no banco de dados",
+          evento: data
+        })
       }
     })
+  }
+
+  getWithParams(req, res) {
+    let limit = parseInt(req.query.limit)
+    let query = {}
+    let page = req.query.page
+    let skip = limit * (page - 1)
+    let {
+      columnSort,
+      valueSort
+    } = req.query
+
+    EventoSchema
+      .find(query)
+      .sort([[columnSort, valueSort]])
+      .skip(skip)
+      .limit(limit)
+      .exec((err, data) => {
+        if (err) {
+          res.status(500).send({ message: 'Houve um erro ao processar sua requisição', err: err })
+        } else {
+          res.status(200).send({ message: 'Dados recuperados com sucesso', data: data, page: page, limit: limit })
+        }
+      })
   }
 }
 
