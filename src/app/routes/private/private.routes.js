2015d4f87508c6439a00121d7a031cb235bc7c7e
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-13 16:40:35 -0300

    Importando as rotas categories.private para private.routes

--- src/app/routes/private/private.routes.js
+++ src/app/routes/private/private.routes.js
@@ -5,10 +5,12 @@ const UserRoutes = require('./routes/usuario.private')
 const ValidatorRoutes = require('./routes/validators.private')
 const AnalyticsRoutes = require('./routes/analytics.private')
 const PublicacoesRoutes = require('./routes/publicacoes.private')
+const CategoriesRoutes = require('./routes/categories.private')
 
 app.use('/usuario', UserRoutes)
 app.use('/validators', ValidatorRoutes)
 app.use('/analytics', AnalyticsRoutes)
 app.use('/publicacoes', PublicacoesRoutes)
+app.use('/categories', CategoriesRoutes)
 
 module.exports = app
