3c637295a9a50dbc3e0b8067d4bef8bea95af11e
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-14 01:10:05 -0300

    Changelog correção

--- src/app/routes/private/routes/publicacoes.private.js
+++ src/app/routes/private/routes/publicacoes.private.js
@@ -2,11 +2,7 @@ const express = require('express')
 const route = express.Router()
 const Publicacoes = require('./../../../controllers/publicacoes')
 
-<<<<<<< HEAD
-route.post('/create', Publicacoes.create)
-=======
 route.post('/criar', Publicacoes.create)
 route.get('/listar-todos', Publicacoes.getWithParams)
->>>>>>> 72dfc69... Implementando o método getWithParams e a rota listar-todos para corrigir um erro
 
 module.exports = route
