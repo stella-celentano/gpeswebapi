72dfc69c78aedc1e759e9f69165c4e123e9dcc3c
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-14 01:07:10 -0300

    Implementando o método getWithParams e a rota listar-todos para corrigir um erro

--- src/app/routes/private/routes/publicacoes.private.js
+++ src/app/routes/private/routes/publicacoes.private.js
@@ -3,5 +3,6 @@ const route = express.Router()
 const Publicacoes = require('./../../../controllers/publicacoes')
 
 route.post('/criar', Publicacoes.create)
+route.get('/listar-todos', Publicacoes.getWithParams)
 
 module.exports = route
