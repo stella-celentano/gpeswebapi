e633ba888bc447a24c44206a08b245e88daf103b
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-14 01:07:10 -0300

    Implementando o método getWithParams e a rota listar-todos para corrigir um erro

--- src/app/controllers/publicacoes.js
+++ src/app/controllers/publicacoes.js
@@ -12,6 +12,66 @@ class Publicacoes {
     })
   }
 
+  getWithParams(req, res) {
+
+    const limit = 10
+
+    let query = {}
+    let page = req.query.page
+    let skip = limit * (page - 1)
+    let { keyword, category, columnSort, valueSort } = req.query
+
+    if (category) {
+      query['tipo'] = new RegExp(category, "i")
+    }
+
+    if (keyword) {
+      query = { $text: { $search: `"\"${keyword}\""` } }
+    }
+
+    publicacoesSchema
+      .find(query)
+      .sort([[columnSort, valueSort]])
+      .skip(skip)
+      .limit(limit)
+      .exec((err, data) => {
+        if (err) {
+          res.status(500).json({ message: 'Houve um erro ao processar sua requisição', err: err })
+        }
+        else if (Array.isArray(data) && data.length == 0) {
+          res.status(200).json({ message: 'Não foram encontrados dados para os termos da pesquisa! Tente pesquisar novamente' })
+        } else {
+          publicacoesSchema
+            .estimatedDocumentCount()
+            .find(query)
+            .exec((err, count) => {
+              let totalDocuments = count.length
+              if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', err: err })
+              } else {
+                if (totalDocuments > 0) {
+                  res.status(200).json({
+                    message: 'Dados recuperados com sucesso',
+                    data: data,
+                    page: page,
+                    limit: limit,
+                    count: totalDocuments,
+                  })
+                } else {
+                  res.status(204).json({
+                    message: 'Não há dados para serem exibidos',
+                    data: data,
+                    page: page,
+                    limit: limit,
+                    count: totalDocuments,
+                  })
+                }
+              }
+            })
+        }
+      })
+  }
+
 }
 
 module.exports = new Publicacoes()
