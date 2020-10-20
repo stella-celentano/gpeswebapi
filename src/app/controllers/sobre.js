9feabedab442d07dd0ed7701331d74dfcc00c2f5
roodev <rodrigo.fessore@gmail.com>
2020-10-20 20:02:31 -0300

    Padronizando a mensagem de sucesso na criação do sobre

--- src/app/controllers/sobre.js
+++ src/app/controllers/sobre.js
@@ -34,9 +34,9 @@ class Sobre {
 
         SobreSchema.create(body, (err, data) => {
             if (err) {
-                res.status(500).send({ messa: 'Houve um erro ao processar sua requisição', error: err })
+                res.status(500).send({ message: 'Houve um erro ao processar sua requisição', error: err })
             } else {
-                res.status(201).send({ message: 'Sobre criado com sucesso no banco de dados', sobre: data })
+                res.status(201).send({ message: 'Sobre criado com sucesso', sobre: data })
             }
         })
     }
