2c77aa7c01f299fd9ec0aac9ff59b15035f19f11
roodev <rodrigo.fessore@gmail.com>
2020-10-21 01:23:33 -0300

    Implementando busca por título, rotas e atualizando o change.log

--- src/app/routes/private/routes/sobre.private.js
+++ src/app/routes/private/routes/sobre.private.js
@@ -4,6 +4,7 @@ const SobreController = require('./../../../controllers/sobre')
 
 route.get('/listar-todos', SobreController.getWithParams)
 route.post('/criar', SobreController.create)
+route.get('/listar-um/:title', SobreController.getByTitle);
 
 
 module.exports = route
