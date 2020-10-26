2598b3d9a17e95ccbf32f72911765deec7fee38a
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-10-26 07:43:50 -0300

    Criando arquivo de rotas publicas de publicacões e disponibilizando em public.routes

--- src/app/routes/public/public.routes.js
+++ src/app/routes/public/public.routes.js
@@ -4,9 +4,11 @@ const app = express();
 const AuthenticationRoutes = require('./routes/authentication.public');
 const ContatoRoutes = require('./routes/contato.public');
 const ProcessoSeletivoRoutes = require('./routes/processo-seletivo.public');
+const PublicacoesRoutes = require('./routes/publicacoes.public');
 
 app.use('/authentication', AuthenticationRoutes);
 app.use('/contato', ContatoRoutes);
 app.use('/processo-seletivo', ProcessoSeletivoRoutes);
+app.use('/publicacoes', PublicacoesRoutes)
 
 module.exports = app
