7559f194104059c4e9050b45def363c146dc943f
stella-celentano <stellacelentano99@gmail.com>
2020-10-26 13:08:51 -0300

    Implementando método que exclui um registro

--- src/app/controllers/processo_seletivo.js
+++ src/app/controllers/processo_seletivo.js
@@ -92,6 +92,16 @@ class ProcessoSeletivo {
             }
         })
     }
+
+    delete(req, res) {
+        ProcessoSeletivoSchama.deleteOne({ _id: req.params.id }, (err, data) => {
+            if (err) {
+                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+            } else {
+                res.status(200).json({ message: 'Processo Seletivo apagado com sucesso', data: data })
+            }
+        })
+    }
 }
 
 module.exports = new ProcessoSeletivo()
