e6c3a38606d1d12eca930f42d93e57aefea8a427
stella-celentano <stellacelentano99@gmail.com>
2020-10-26 15:28:45 -0300

    Implementando rota pública para listar os registros

--- src/app/routes/public/public.routes.js
+++ src/app/routes/public/public.routes.js
@@ -1,10 +1,12 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const AuthenticationRoutes = require('./routes/authentication.public')
-const ContatoRoutes = require('./routes/contato.public')
+const AuthenticationRoutes = require('./routes/authentication.public');
+const ContatoRoutes = require('./routes/contato.public');
+const ProcessoSeletivoRoutes = require('./routes/processo-seletivo.public');
 
-app.use('/authentication', AuthenticationRoutes)
-app.use('/contato', ContatoRoutes)
+app.use('/authentication', AuthenticationRoutes);
+app.use('/contato', ContatoRoutes);
+app.use('/processo-seletivo', ProcessoSeletivoRoutes);
 
 module.exports = app
