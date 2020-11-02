01e22044377c5ebcc100e07a1f4ce7b4044154c8
stella-celentano <stellacelentano99@gmail.com>
2020-11-02 14:28:56 -0300

    Comentando as requisições ao analytics pois chegou ao limite de solicitações à API do Google Analytics

--- src/app/routes/private/private.routes.js
+++ src/app/routes/private/private.routes.js
@@ -14,7 +14,7 @@ const SobreRoutes= require('./routes/sobre.private')
 
 app.use('/usuario', UserRoutes)
 app.use('/validators', ValidatorRoutes)
-app.use('/analytics', AnalyticsRoutes)
+// app.use('/analytics', AnalyticsRoutes)
 app.use('/contato', ContatoRoutes)
 app.use('/processo-seletivo', ProcessoSeletivo)
 app.use('/integrantes', IntegrantesRoutes)
