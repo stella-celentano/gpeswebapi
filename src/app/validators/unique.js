be0ae7e0baec135738f0f8d4511657f29cb6568a
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-04 02:21:15 -0300

    Implementando validator unique name autores

--- src/app/validators/unique.js
+++ src/app/validators/unique.js
@@ -6,6 +6,7 @@ const CategoriaSchema = require('./../models/categories');
 const PublicacoesSchema = require('./../models/publicacoes');
 const EventoSchema = require('./../models/eventos');
 const SobreSchema = require('./../models/sobre');
+const AutoresSchema = require('./../models/autores');
 
 
 class UniqueValidators {
@@ -176,6 +177,23 @@ class UniqueValidators {
                     res.status(200).json({ message: "Já existe um documento com esse título.", result: result.length })
                 } else {
                     res.status(200).json({ message: "Categoria disponível.", result: result.length })
+                }
+            }
+        })
+    }
+
+    /**Função para validar se o nome do autor que está sendo criado é único. */
+    uniqueAutorNome(req, res) {
+        const nome = req.query.name.replace(/%20/g, " ")
+
+        AutoresSchema.find({ nome: { '$regex': `^${nome}$`, '$options': 'i' } }, function (err, result) {
+            if (err) {
+                res.status(500).json({ message: "Houve um erro ao processar sua requisição.", error: err })
+            } else {
+                if (result.length > 0) {
+                    res.status(200).json({ message: "Já existe um documento com esse título.", result: result.length })
+                } else {
+                    res.status(200).json({ message: "Autor disponível.", result: result.length })
                 }
             }
         })
