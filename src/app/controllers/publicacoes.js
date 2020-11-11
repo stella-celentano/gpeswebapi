35329bd29a301c05dd411bc067286a5102cf350d
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-11 10:25:19 -0300

    Implementando o método delete

--- src/app/controllers/publicacoes.js
+++ src/app/controllers/publicacoes.js
@@ -113,6 +113,16 @@ class Publicacoes {
       })
   }
 
+  delete(req, res) {
+    publicacoesSchema.deleteOne({ _id: req.query.id }, (err, data) => {
+      if (err) {
+        res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+      } else {
+        res.status(200).json({ message: 'Publicação apagada com sucesso', data: data })
+      }
+    })
+  }
+
 }
 
 module.exports = new Publicacoes()
