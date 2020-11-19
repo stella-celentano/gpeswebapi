292a057e8c52efff7b87f15f4f48b11b50c57626
stella-celentano <stellacelentano99@gmail.com>
2020-11-19 13:12:48 -0300

    Implementando método que busca todos os registros de selecao e disponibilizando rota privada

--- src/app/routes/private/routes/selecao.private.js
+++ src/app/routes/private/routes/selecao.private.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const Selecao = require('./../../../controllers/selecao');
 
+route.get('/listar-todos', Selecao.getWithParams);
 route.post('/criar', Selecao.create);
 
 module.exports = route
