b5cae4aba73a191b6f1a2abf52fa7c10663d152a
stella-celentano <stellacelentano99@gmail.com>
2020-11-19 13:06:12 -0300

    Adaptando a inscricao para receber o relacionamento com selecao

--- src/app/subs/inscricao.js
+++ src/app/subs/inscricao.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const handlebars = require('handlebars');
 const nodemailer = require('nodemailer');
 const InscricaoSchema = require('./../models/inscricao');
+const SelecaoSchema = require('./../models/selecao');
 
 const readHTMLFile = function (path, callback) {
     fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
@@ -28,40 +29,56 @@ const smtpTransport = nodemailer.createTransport({
 })
 
 exports.sendMailConfirmSubscribe = function (req, res) {
-    InscricaoSchema.create(req.body, (err, data) => {
+    const reqBody = req.body;
+    const idSelecao = reqBody['selecao'];
+
+    InscricaoSchema.create(reqBody, (err, data) => {
         if (err) {
             res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
         } else {
-            const email = data.email
-           
-            readHTMLFile(__dirname + '/template/confirm-subs.html', function (err, html) {
+            SelecaoSchema.findById(idSelecao, (err, selecao) => {
                 if (err) {
-                    console.log(`Erro ao ler o HTML => ${err}`)
+                    res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
                 } else {
-                    /**Compila o arquivo HTML utilizando handlebars */
-                    let template = handlebars.compile(html)
-                    /**Processa os dados que serão recolocados no template */
-                    let replacements = {
-                        url: process.env.MESSENGER_URL_CREATE,
-                        name: data.nome
-                    }
-                    /**Adiciona os dados recolocados no template */
-                    let htmlToSend = template(replacements)
-                    /**Define para quem será enviado o email, de qual email, qual corpo da mensagem e qual assunto da mensagem */
-                    let mailOptions = {
-                        to: email,
-                        from: process.env.MESSENGER_MAIL,
-                        html: htmlToSend,
-                        subject: 'Confirmação de Inscrição | GPES'
-                    }
-
-                    /**Realiza o envio do e-mail */
-                    smtpTransport.sendMail(mailOptions, function (err) {
+                    selecao.inscritos.push(data)
+                    selecao.save({}, (err) => {
                         if (err) {
-                            console.log(err)
-                            res.status(442).json({ message: "Serviço indisponível" })
+                            res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
                         } else {
-                            res.status(201).json({ message: 'Inscrição criada com sucesso', data: data })
+                            const email = data.email
+
+                            readHTMLFile(__dirname + '/template/confirm-subs.html', function (err, html) {
+                                if (err) {
+                                    console.log(`Erro ao ler o HTML => ${err}`)
+                                } else {
+                                    /**Compila o arquivo HTML utilizando handlebars */
+                                    let template = handlebars.compile(html)
+                                    /**Processa os dados que serão recolocados no template */
+                                    let replacements = {
+                                        url: process.env.MESSENGER_URL_CREATE,
+                                        name: data.nome
+                                    }
+                                    /**Adiciona os dados recolocados no template */
+                                    let htmlToSend = template(replacements)
+                                    /**Define para quem será enviado o email, de qual email, qual corpo da mensagem e qual assunto da mensagem */
+                                    let mailOptions = {
+                                        to: email,
+                                        from: process.env.MESSENGER_MAIL,
+                                        html: htmlToSend,
+                                        subject: 'Confirmação de Inscrição | GPES'
+                                    }
+
+                                    /**Realiza o envio do e-mail */
+                                    smtpTransport.sendMail(mailOptions, function (err) {
+                                        if (err) {
+                                            console.log(err)
+                                            res.status(442).json({ message: "Serviço indisponível" })
+                                        } else {
+                                            res.status(201).json({ message: 'Inscrição criada com sucesso', data: data })
+                                        }
+                                    })
+                                }
+                            })
                         }
                     })
                 }
