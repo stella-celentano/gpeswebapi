6e9cdb6b32c47ca8b464fdf9a3a56c3ac81b2a1d
stella-celentano <stellacelentano99@gmail.com>
2020-11-19 13:05:08 -0300

    Criando a  model de selecao

---
+++ src/app/models/selecao.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const SelecaoSchema = new Schema({
+    titulo: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    dataInicio: {
+        type: Date,
+        required: true
+    },
+    dataFim: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    inscritos: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'inscricaoSchema'
+        }
+    ]
+},
+    {
+        versionKey: false,
+        timestamps: false
+    }
+)
+
+module.exports = model('selecaoSchema', SelecaoSchema)
