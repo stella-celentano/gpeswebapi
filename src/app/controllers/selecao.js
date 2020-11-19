1ef1056d0a071f7012a3a759148ab049913d8212
stella-celentano <stellacelentano99@gmail.com>
2020-11-19 15:40:28 -0300

    Implementando método update garantindo que o campo dataInicio não seja alterado e disponibilizando rota privada

--- src/app/controllers/selecao.js
+++ src/app/controllers/selecao.js
@@ -49,14 +49,14 @@ class Selecao {
         let title = req.params.title.replace(/%20/g, " ")
 
         SelecaoSchema.findOne({ titulo: { $eq: title } })
-        .populate('inscritos', {nome: 1, email: 1, curso: 1, semestre: 1})
-        .exec((err, data) => {
-            if (err) {
-                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
-            } else {
-                res.status(200).json({ message: 'Processo Seletivo recuperado com sucesso', data: data })
-            }
-        })
+            .populate('inscritos', { nome: 1, email: 1, curso: 1, semestre: 1 })
+            .exec((err, data) => {
+                if (err) {
+                    res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+                } else {
+                    res.status(200).json({ message: 'Processo Seletivo recuperado com sucesso', data: data })
+                }
+            })
     }
 
     create(req, res) {
@@ -67,6 +67,23 @@ class Selecao {
                 res.status(201).json({ message: 'Seleção aberta com sucesso', data: data })
             }
         })
+    }
+
+    update(req, res) {
+        let title = req.params.title.replace(/%20/g, " ")
+        let body = req.body
+
+        if (body['dataInicio']) {
+            res.status(403).json({ message: 'Não é possível alterar o campo Data Início' });
+        } else {
+            SelecaoSchema.updateOne({ titulo: title }, { $set: body }, (err, data) => {
+                if (err) {
+                    res.status(500).json({ message: 'Houve um erro ao processar sua requisição', error: err })
+                } else {
+                    res.status(201).json({ message: 'Seleção atualizada com sucesso', data: data })
+                }
+            })
+        }
     }
 }
 
--- src/app/routes/private/routes/selecao.private.js
+++ src/app/routes/private/routes/selecao.private.js
@@ -4,6 +4,7 @@ const Selecao = require('./../../../controllers/selecao');
 
 route.get('/listar-todos', Selecao.getWithParams);
 route.get('/listar-um/:title', Selecao.getByTitle);
+route.put('/atualizar/:title', Selecao.update);
 route.post('/criar', Selecao.create);
 
 module.exports = route
