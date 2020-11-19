c0cf4a301fe26cf329a6bdbd98cc43ed8bded469
stella-celentano <stellacelentano99@gmail.com>
2020-11-19 13:12:48 -0300

    Implementando método que busca todos os registros de selecao e disponibilizando rota privada

--- src/app/controllers/selecao.js
+++ src/app/controllers/selecao.js
@@ -1,6 +1,50 @@
 const SelecaoSchema = require('./../models/selecao');
 
 class Selecao {
+
+    getWithParams(req, res) {
+
+        let limit = parseInt(req.query.limit)
+        let query = {}
+        let page = req.query.page
+        let skip = limit * (page - 1)
+        let { columnSort, valueSort } = req.query
+
+        SelecaoSchema
+            .find(query)
+            .sort([[columnSort, valueSort]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, data) => {
+                if (err) {
+                    res.status(500).json({ message: 'Houve um erro ao processar sua requisição', err: err })
+                } else {
+                    SelecaoSchema
+                        .find(query)
+                        .exec((err, count) => {
+                            let totalDocuments = count.length
+                            if (err) {
+                                res.status(500).json({ message: 'Houve um erro ao processar sua requisição', err: err })
+                            } else {
+                                if (totalDocuments > 0) {
+                                    res.status(200).json({
+                                        message: 'Dados recuperados com sucesso',
+                                        data: data,
+                                        page: page,
+                                        limit: limit,
+                                        count: totalDocuments,
+                                    })
+                                } else {
+                                    res.status(204).json({
+                                        message: 'Não há dados para serem exibidos'
+                                    })
+                                }
+                            }
+                        })
+                }
+            })
+    }
+
     create(req, res) {
         SelecaoSchema.create(req.body, (err, data) => {
             if (err) {
