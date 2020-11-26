f64d9808f4abc6320349a8cadcf78bd9af4485bf
stella-celentano <stellacelentano99@gmail.com>
2020-11-26 14:22:47 -0300

    Implementando método que faz a busca do processo seletivo que está aberto verificando as datas e status

--- src/app/routes/public/routes/processo-seletivo.public.js
+++ src/app/routes/public/routes/processo-seletivo.public.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const route = express.Router();
 const ProcessoSeletivoController = require('./../../../controllers/processo_seletivo');
-
 const InscricaoController = require('./../../../subs/inscricao');
+const SelecaoController = require('./../../../controllers/selecao');
 
 route.get('/listar-todos', ProcessoSeletivoController.getWithParams);
-
+route.get('/selecao-aberta', SelecaoController.getSelecaoAberta);
 route.post('/criar', InscricaoController.sendMailConfirmSubscribe);
 
 module.exports = route;
