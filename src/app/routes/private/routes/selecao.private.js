c8ae4ec40b31853352ac6dde1ae76a700030c265
carl0sR0ma0 <cvpromao@gmail.com>
2021-03-14 18:57:37 -0300

    Implementando a funcionalidade de enviar e-mails para os inscritos

--- src/app/routes/private/routes/selecao.private.js
+++ src/app/routes/private/routes/selecao.private.js
@@ -7,6 +7,7 @@ route.get('/listar-um/:title', Selecao.getByTitle);
 route.get('/listar-um-inscrito/:name', Selecao.getByNameInscrito);
 route.put('/atualizar/:title', Selecao.update);
 route.post('/criar', Selecao.create);
+route.post('/listar-um/:title/enviar-email', Selecao.sendEmail);
 route.delete('/apagar/:id', Selecao.delete);
 
 module.exports = route
